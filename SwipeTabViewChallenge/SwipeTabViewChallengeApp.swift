d22e31cb05aa6dedbb2ef713ee4e53ac37ec0e16
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2021-10-19 10:28:36 -0400

    Initial Commit

---
+++ SwipeTabViewChallenge/SwipeTabViewChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeTabViewChallengeApp.swift
+//  SwipeTabViewChallenge
+//
+//  Created by Jeffrey Sweeney on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct SwipeTabViewChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CardSwiperView()
+        }
+    }
+}
