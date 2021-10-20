4527b77af94421c5a78c04044a6446f2c2f8e0fb
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2021-10-20 14:59:24 -0400

    Initial Commit

---
+++ PickerChallenge/PickerChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerChallengeApp.swift
+//  PickerChallenge
+//
+//  Created by Jeffrey Sweeney on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct PickerChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PickerView()
+        }
+    }
+}
