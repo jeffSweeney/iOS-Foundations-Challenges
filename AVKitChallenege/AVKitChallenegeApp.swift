bbfabed812dc06fe7467790540a5f9e8c1e7bb80
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2021-10-30 15:34:31 -0400

    Initial Commit

---
+++ AVKitChallenege/AVKitChallenegeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVKitChallenegeApp.swift
+//  AVKitChallenege
+//
+//  Created by Jeffrey Sweeney on 10/30/21.
+//
+
+import SwiftUI
+
+@main
+struct AVKitChallenegeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
