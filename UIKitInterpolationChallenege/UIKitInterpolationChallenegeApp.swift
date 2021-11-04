7b564a02a44b8928c01885cd62907a79ba423ca5
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2021-11-04 10:42:19 -0400

    Initial Commit

---
+++ UIKitInterpolationChallenege/UIKitInterpolationChallenegeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitInterpolationChallenegeApp.swift
+//  UIKitInterpolationChallenege
+//
+//  Created by Jeffrey Sweeney on 11/4/21.
+//
+
+import SwiftUI
+
+@main
+struct UIKitInterpolationChallenegeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
