09d72b3f205a4d5f7858ef1bf433c42165b79d25
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2021-11-13 20:14:45 -0500

    Initial Commit

---
+++ RandomFactChallenge_iOSNetworking/RandomFactChallenge_iOSNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomFactChallenge_iOSNetworkingApp.swift
+//  RandomFactChallenge_iOSNetworking
+//
+//  Created by Jeffrey Sweeney on 11/13/21.
+//
+
+import SwiftUI
+
+@main
+struct RandomFactChallenge_iOSNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
